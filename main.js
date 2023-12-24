a1e26fbf801dfe652acefc2172575eea1d938faa
zcnxj <151899280+zcnxj@users.noreply.github.com>
2023-12-24 09:27:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ btn.addEventListener("click", function () {
   var name = document.getElementById("myName").value;
   alert("Name: " + name);
 });
+if ('seviceworker' in navigator) {
+  window.addEventlistener('load', function){
+    navigator.seviceworker.register('sw.js');
+  }
+}
