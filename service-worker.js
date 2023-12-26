e89fcbfda44e2bf3ab1171116d52ffdc97a2c385
zcnxj <151899280+zcnxj@users.noreply.github.com>
2023-12-26 08:06:36 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+//service-worker.js
+const CACHE_NAME = 'example-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/scripts/main.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
